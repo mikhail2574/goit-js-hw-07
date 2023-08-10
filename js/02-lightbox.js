749b9d7c675eb9a89353a9ce4c0d2caca50a1807
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2023-08-10 13:51:47 +0300

    fixed bug in scope in 2nd task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,7 +8,7 @@ const markup = galleryItems
   .map((item) => {
     const { preview, original, description } = item;
     return `<li class="gallery__item">
-   <a class="gallery__link" href="${preview}">
+   <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}" />
    </a>
 </li>`;
